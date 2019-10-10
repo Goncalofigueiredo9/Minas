6ecfe012befedbb09782067dc871dcca932b5508
Gonçalo Figueiredo <Gonalo Figueiredo@WIN-GESFLHLR4EE>
2019-10-10 10:46:10 +0100

    Adicionar arquivos de projeto.

---
+++ Minas/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Minas")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Minas")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("ddf5fb44-701f-47a6-b971-ebd01cd696ae")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
